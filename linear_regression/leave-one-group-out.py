f434a1307037e48321e4c55278ccf27f3acbc294
tkmru <i.am.tkmru@gmail.com>
2017-10-07 21:02:19 +0900

    add leave-one-group-out

--- linear_regression/leave-one-group-out.py
+++ linear_regression/leave-one-group-out.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # coding: UTF-8
 
+import numpy as np
 from sklearn.datasets import load_breast_cancer
-from sklearn.model_selection import LeaveOneOut, cross_val_score
+from sklearn.model_selection import LeaveOneGroupOut, cross_val_score
 from sklearn.linear_model import LogisticRegression
 
 data = load_breast_cancer()
@@ -10,10 +11,14 @@ data = load_breast_cancer()
 x = data.data
 y = data.target
 
-loocv = LeaveOneOut()
-train_index, test_index = next(loocv.split(x, y))
+# data don't have group, group is made here
+group = np.array(list(range(50))*12)
+group = np.sort(group[:y.size])
+
+loocv = LeaveOneGroupOut()
 clf = LogisticRegression()
-ave_score = cross_val_score(clf, x, y, cv=loocv)
+
+ave_score = cross_val_score(clf, x, y, groups=group, cv=loocv)
 
 print('size: {0}'.format(ave_score.size))
 print('{0:4.2f} +/- {1:4.2f} %'.format(ave_score.mean() * 100, ave_score.std() * 100))
