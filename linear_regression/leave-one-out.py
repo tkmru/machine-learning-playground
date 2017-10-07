15525682d27c17a58b643bb85b279e340ef50a9c
tkmru <i.am.tkmru@gmail.com>
2017-10-07 20:50:34 +0900

    add leave-one-out

--- linear_regression/leave-one-out.py
+++ linear_regression/leave-one-out.py
@@ -2,18 +2,17 @@
 # coding: UTF-8
 
 from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import LeaveOneOut, cross_val_score
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import StratifiedKFold, cross_val_score
 
 data = load_breast_cancer()
 
 x = data.data
 y = data.target
 
+loocv = LeaveOneOut()
+train_index, test_index = next(loocv.split(x, y))
 clf = LogisticRegression()
-
-# K-fold cross-validation: the average of the values computed in the loop
-s = StratifiedKFold(n_splits=10, shuffle=True)
-ave_score = cross_val_score(clf, x, y, cv=10)
+ave_score = cross_val_score(clf, x, y, cv=loocv)
 
 print('{0:4.2f} +/- {1:4.2f} %'.format(ave_score.mean() * 100, ave_score.std() * 100))
