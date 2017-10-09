0beed7e1409470dc98ba29288d81c18cd253b5f1
tkmru <i.am.tkmru@gmail.com>
2017-10-10 08:38:50 +0900

    add KNeighborsClassifier

---
+++ k_neighbors_classifier.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# coding: UTF-8
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(cancer.data,
+    cancer.target, stratify=cancer.target, random_state=66)
+
+training_accuracy = []
+test_accuracy = []
+neighbors_settings = range(1, 11)
+
+'''
+k <= 2 overfitting
+k >= 7 accurate is down
+k = 6 best
+'''
+
+for n_neighbors in neighbors_settings:
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf.fit(X_train, y_train)
+    training_accuracy.append(clf.score(X_train, y_train))
+    test_accuracy.append(clf.score(X_test, y_test))
+
+
+plt.plot(neighbors_settings, training_accuracy, label="training_accuracy")
+plt.plot(neighbors_settings, test_accuracy, label="test_accuracy")
+plt.legend()
+plt.show()
